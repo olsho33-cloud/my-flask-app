810aa9570e470c5b23c9e8e73ed67f317d5560dc
olsho33-cloud <olsho33@gmail.com>
2026-09-22 20:39:51 +0900

    Change message for DevOps

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ r = redis.Redis(host="redis", port=6379)
 @app.route("/")
 def home():
     count = r.incr("count")
-    return f"Hello Cloud Engineer! Visit count: {count}"
+    return f"Hello DevOps Engineer! Visit count: {count}"
 
 app.run(host="0.0.0.0", port=5000)
