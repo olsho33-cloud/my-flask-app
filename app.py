a569b6826bad2dcaa4eacbc53cff18427cd5b43d
olsho33-cloud <olsho33@gmail.com>
2026-09-22 20:25:16 +0900

    Initail Flask Docker app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import redis
+
+app = Flask(__name__)
+
+r = redis.Redis(host="redis", port=6379)
+
+@app.route("/")
+def home():
+    count = r.incr("count")
+    return f"Hello AWS! Visit count: {count}"
+
+app.run(host="0.0.0.0", port=5000)
